d6570957a9e45b48b1a37693ae1baaf654ab6680
seabat <ryouta.nakanishi.86@gmail.com>
2023-04-01 22:35:14 +0900

    fat な Activity#onCreate をスリムにする

--- app/src/main/java/dev/seabat/android/loginvalid/ui/login/LoginActivity.kt
+++ app/src/main/java/dev/seabat/android/loginvalid/ui/login/LoginActivity.kt
@@ -18,7 +18,7 @@ import dev.seabat.android.loginvalid.R
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var viewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,74 +27,85 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val username = binding.username
-        val password = binding.password
-        val login = binding.login
-        val loading = binding.loading
-
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+        viewModel = ViewModelProvider(this, LoginViewModelFactory())
             .get(LoginViewModel::class.java)
 
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
+        observeLiveData()
+        setListener()
+    }
+
+    private fun observeLiveData() {
+        val username = binding.edittextUsername
+        val password = binding.edittextPassword
+        val login = binding.buttonLogin
+        val loading = binding.progressbarLoading
+
+        viewModel.loginFormState.observe(this@LoginActivity, Observer {
+            it ?: return@Observer
 
             // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
+            login.isEnabled = it.isDataValid
 
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
+            if (it.usernameError != null) {
+                username.error = getString(it.usernameError)
             }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
+            if (it.passwordError != null) {
+                password.error = getString(it.passwordError)
             }
         })
 
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
+        viewModel.loginResult.observe(this@LoginActivity, Observer {
+            it ?: return@Observer
 
             loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
+            if (it.error != null) {
+                showLoginFailed(it.error)
             }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
+            if (it.success != null) {
+                updateUiWithUser(it.success)
             }
             setResult(Activity.RESULT_OK)
 
             //Complete and destroy login activity once successful
             finish()
         })
+    }
+
+    private fun setListener() {
+        val username = binding.edittextUsername
+        val password = binding.edittextPassword
+        val login = binding.buttonLogin
+        val loading = binding.progressbarLoading
 
         username.afterTextChanged {
-            loginViewModel.loginDataChanged(
+            viewModel.loginDataChanged(
                 username.text.toString(),
                 password.text.toString()
             )
         }
 
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
+        password.also {
+            it.afterTextChanged {
+                viewModel.loginDataChanged(
                     username.text.toString(),
                     password.text.toString()
                 )
             }
-
-            setOnEditorActionListener { _, actionId, _ ->
+            it.setOnEditorActionListener { _, actionId, _ ->
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
+                        viewModel.login(
                             username.text.toString(),
                             password.text.toString()
                         )
                 }
                 false
             }
+        }
 
-            login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
-            }
+        login.setOnClickListener {
+            loading.visibility = View.VISIBLE
+            viewModel.login(username.text.toString(), password.text.toString())
         }
     }
 
