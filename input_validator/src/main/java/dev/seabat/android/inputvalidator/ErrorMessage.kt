d5461ccba0772d49b5f2d803cc3fd3fae915e9db
seabat <ryouta.nakanishi.86@gmail.com>
2023-05-05 22:02:14 +0900

    ErrorMessage kdoc を作成

--- input_validator/src/main/java/dev/seabat/android/inputvalidator/ErrorMessage.kt
+++ input_validator/src/main/java/dev/seabat/android/inputvalidator/ErrorMessage.kt
@@ -1,6 +1,16 @@
 package dev.seabat.android.inputvalidator
 
+import androidx.annotation.StringRes
+
+/**
+ * Validation で不適合と判断された場合のメッセージ
+ *
+ * メッセージは resId と string のどちらかに指定すること
+ *
+ * @param resId アプリの文字列リソースのID
+ * @param string エラーメッセージ
+ */
 data class ErrorMessage(
-    val resId: Int? = null, // アプリのリソースID
-    val string: String? = null // エラーメッセージ
+    @StringRes val resId: Int? = null,
+    val string: String? = null
 )
